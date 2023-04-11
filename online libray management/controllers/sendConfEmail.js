70881000eea452d7e7ec6284778cfe40323fb5c4
Do Dinh Dong <17000@student.vgu.edu.vn>
2023-04-11 10:05:13 +0700

    changed button

--- online libray management/controllers/sendConfEmail.js
+++ online libray management/controllers/sendConfEmail.js
@@ -4,10 +4,13 @@ const jwtSecret='secret secret'
 module.exports=(req,res)=>{
     const {email}=req.body
 
-    User.findOne({email})
-    .then((user)=>{
-        // const secret=jwtSecret+user.password
-        // const payload={email:user.email,id:user.id}
+    //User.findOne({email})
+    //.then((user)=>{
+        //const secret=jwtSecret+user.password
+        const payload={email:user.email,id:user.id}
         console.log(email)
-    })
+        const token=jwt.sign(payload,secret,{expiresIn:'5m'})
+        const link='http://localhost:3000/reset/${user.id}/${token}'
+        res.send('Password reset link has been sent')
+    //})
 }
