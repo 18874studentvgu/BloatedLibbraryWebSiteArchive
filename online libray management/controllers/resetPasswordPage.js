ea3991e7ff87e51ee88444978f6bf0b556bc8660
17637 <17637@student.vgu.edu.vn>
2023-04-13 09:19:50 +0700

    change stuff in resetpasswordpage.js req is defined so the thing keep crashing

--- online libray management/controllers/resetPasswordPage.js
+++ online libray management/controllers/resetPasswordPage.js
@@ -1,10 +1,11 @@
 const jwt=require('jsonwebtoken')
 const User=require('../models/User')
 
-const {id,token}=req.params
-    console.log('checking id')
+
 
 module.exports = (req, res) => {
+    const {id,token}=req.params
+    console.log('checking id')
     User.findOne({id}) //check id, if id exists means that user exists
     .then(()=>{
         const secret=jwtSecret+User.password //this is from sendConfEmail.js
