376c4b6db4c7e307aa835c9d106257a2814347e5
Do Dinh Dong <17000@student.vgu.edu.vn>
2023-04-15 17:23:36 +0700

    Update sendEmail.js

--- online libray management/controllers/sendEmail.js
+++ online libray management/controllers/sendEmail.js
@@ -1,8 +1,28 @@
 const nodemailer=require('nodemailer')
 
-const html=`
+const htmlContent=` //email content
   <h1>Hello</h1>
-  <p>Paragraph</p>
-  `
-  
+  <p>This is a test email</p>
+  `;
 
+  async function main(){
+    const testAcc=await nodemailer.createTestAccount()
+    const transporter=nodemailer.createTransport({
+      host:'smtp.ethereal.email',
+      port:465,
+      secure:true,
+      auth:{
+        user:testAcc.user,
+        pass:testAcc.pass
+      }
+    })
+    const info=await transporter.sendMail({
+      from:'foo@x.com',
+      to:'bar@x.com',
+      subject:'Testing',
+      html:htmlContent,
+    })
+    console.log('Message sent: '+info.messageId)
+    console.log('URL: '+nodemailer.getTestMessageUrl(info))
+  }
+main().catch(e=>console.log(e))
