f23d8f7cf3487bde495495c1f652924e3bb0eba8
Tom Lawton <vnminhduy0750@outlook.com>
2023-05-01 11:47:18 +0700

    Updated front-end files for Admin Page

--- online libray management/controllers/admin/dashboard.js
+++ online libray management/controllers/admin/dashboard.js
@@ -1,3 +1,5 @@
+const path = require('path');
 module.exports = (request, response) => {
     response.render('dashboard')
+    // response.sendFile(path.resolve('../online libray management/public', 'admin.html'))
 }
--- online libray management/controllers/admin/usersList.js
+++ online libray management/controllers/admin/usersList.js
@@ -1,3 +1,5 @@
+const path = require('path');
 module.exports = (request, response) => {
     response.render('usersList')
+    // response.sendFile(path.resolve('../online libray management/public', 'admin-user-board.html'))
 }
