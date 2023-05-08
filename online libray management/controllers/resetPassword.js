4571e7ec6a572872f6d26ba6ecb01bede83bdd10
Do Dinh Dong <17000@student.vgu.edu.vn>
2023-05-08 15:01:14 +0700

    reset password *almost* done

--- online libray management/controllers/resetPassword.js
+++ online libray management/controllers/resetPassword.js
@@ -2,32 +2,34 @@ const User = require("../models/User")
 const jwt=require('jsonwebtoken')
 const config=require('../controllers/config')
 const userid=require('../controllers/resetPasswordPage')
+const bcrypt=require('bcrypt')
 //user clicks "reset password"
 module.exports=(req,res)=>{
-    console.log(userid.userid)
-    console.log(config.secret)
+    console.log(global.userid)
     const{password,password2}=req.body
-    User.findOne({_id:userid})
+    User.findOne({_id:global.userid})
     .then((user)=>{
         console.log('User exist')
         const secret=config.secret+user.password
+        console.log(secret)
         try{
-            const verify=jwt.verify(token,secret)
+            //const verify=jwt.verify(token,secret)
             if(password==password2){
+                console.log('comparing')
                 const encryptedPassword = bcrypt.hash(password, 10)
                 User.updateOne(
-                    {_id:id},
+                    {_id:global.userid},
                     {$set:{password:encryptedPassword}}
                 )
-                res.redirect('/login')
+                res.redirect('/auth/login')
             }
             else{
                 console.log('Password not match, try again')
                 res.render('resetPasswordPage')
             }
         }
-        catch{
-            console.error()
+        catch(error){
+            console.log(error)
         }
     })
     .catch(()=>{
--- online libray management/index.js
+++ online libray management/index.js
@@ -47,7 +47,9 @@ app.use('*', (req, res, next) => {
 
 });
 
-
+//global.userid=resetPasswordPageController.userid
+global.userid = null;
+console.log(userid)
 
     
 
