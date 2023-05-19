3116f7aaea314c899952c5c493bb23e0bec75fd3
Quang <17452@student.vgu.edu.vn>
2023-05-20 01:15:53 +0700

    change from name to title

--- online libray management/index.js
+++ online libray management/index.js
@@ -243,7 +243,7 @@ app.get('/bookcart', bookcart)
 // get book for searching
 app.post('/getBooks',async (req,res)=>{
     let payload = req.body.payload.trim();
-    let search = await Book.find({name :{$regex : new RegExp('^'+payload+'.*','i')}}).exec();
+    let search = await Book.find({title :{$regex : new RegExp('^'+payload+'.*','i')}}).exec();
     //Limit Search Results to 10
     search = search.slice(0,10);
     res.send({payload: search});
