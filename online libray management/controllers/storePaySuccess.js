ca455f8821bb74e812d9614b73f11fa55fc6cb3d
HoangKim1504 <101057915+HoangKim1504@users.noreply.github.com>
2023-05-21 17:16:42 +0700

    Remove console.log of storePaySuccess

--- online libray management/controllers/storePaySuccess.js
+++ online libray management/controllers/storePaySuccess.js
@@ -2,8 +2,6 @@ const BorrowedBookSchema = require('../models/BorrowRecord')
 module.exports=(req,res) =>{
     BorrowedBookSchema.findByIdAndUpdate(req.params.id, { paymentAmount : 0 })
     .then((remain)=>{
-        console.log(req.body)
-        console.log(remain)
         console.log('Pay success')
         res.redirect('/paySuccess/' + req.params.id)
     })
